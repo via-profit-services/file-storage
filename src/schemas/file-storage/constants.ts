d5181b85957d266719bee45109c4843a5982ab47
Vasily Novosad <delhsmail@gmail.com>
2020-05-27 22:03:25 +0500

    Build

--- src/schemas/file-storage/constants.ts
+++ src/schemas/file-storage/constants.ts
@@ -2,7 +2,7 @@ import {
   LOG_MAZ_FILES, LOG_MAZ_SIZE, LOG_DATE_PATTERNT, LOG_FILENAME_DEBUG, LOG_FILENAME_ERRORS,
 } from '@via-profit-services/core';
 
-export const TEMPORARY_FILE_EXPIRED_AT_SEC = 60 * 5;
+export const TEMPORARY_FILE_EXPIRED_AT_SEC = 1000 * 60;
 export const CRON_JOB_CLEAR_CACHE_NAME = 'fileStorageClearCache';
 export const REDIS_CACHE_NAME = 'fileStorageCache';
 export const LOG_FILENAME_FILES_STORAGE = 'file-storage-%DATE%.log';
