e1cc304918c7be2826242416ca10314bcf6b5e63
Vasily Novosad <delhsmail@gmail.com>
2020-12-24 08:47:30 +0500

    Fixed multiple invocations of cache cleaning

--- src/middleware-factory.ts
+++ src/middleware-factory.ts
@@ -30,28 +30,32 @@ const middlewareFactory: FileStorageMiddlewareFactory = (configuration) => {
     const { services, logger } = pool.context;
     const { cacheTTL, temporaryTTL } = services.files.getProps();
 
-    services.files.clearExpiredCacheFiles();
-    services.files.clearExpiredTemporaryFiles();
+    // setup it once
+    if (!timers.cacheTimer) {
+      services.files.clearExpiredCacheFiles();
+      timers.cacheTimer = setInterval(() => {
+        try {
+          services.files.clearExpiredCacheFiles();
+        } catch (err) {
+          logger.files.error('Failed to clear expired cache files by interval', { err });
+        }
+      }, cacheTTL * 1000);
+      logger.files.info(`A timer is set for clearing the cache for ${cacheTTL} sec.`);
+    }
 
-    clearInterval(timers.cacheTimer);
-    timers.cacheTimer = setInterval(() => {
-      try {
-        services.files.clearExpiredCacheFiles();
-      } catch (err) {
-        logger.files.error('Failed to clear expired cache files by interval', { err });
-      }
-    }, cacheTTL * 1000);
-    logger.files.info(`A timer is set for clearing the cache for ${cacheTTL} sec.`);
-
-    clearInterval(timers.temporaryTimer);
-    timers.temporaryTimer = setInterval(() => {
-      try {
+    // setup it once
+    if (!timers.temporaryTimer) {
+      timers.temporaryTimer = setInterval(() => {
         services.files.clearExpiredTemporaryFiles();
-      } catch (err) {
-        logger.files.error('Failed to clear expired temporary files by interval', { err });
-      }
-    }, temporaryTTL * 1000);
-    logger.files.info(`A timer is set for clearing the temporary for ${temporaryTTL} sec.`);
+        try {
+          services.files.clearExpiredTemporaryFiles();
+        } catch (err) {
+          logger.files.error('Failed to clear expired temporary files by interval', { err });
+        }
+      }, temporaryTTL * 1000);
+      logger.files.info(`A timer is set for clearing the temporary for ${temporaryTTL} sec.`);
+    }
+
 
     return pool;
   }
