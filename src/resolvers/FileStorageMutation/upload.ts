c0c013c67242b3e906d007e76b2476d8f6eb9ed0
Vasily Novosad <delhsmail@gmail.com>
2021-04-29 13:15:02 +0500

    Prevent errors when uploaded file already exist

--- src/resolvers/FileStorageMutation/upload.ts
+++ src/resolvers/FileStorageMutation/upload.ts
@@ -1,10 +1,26 @@
+import { BadRequestError } from '@via-profit-services/core';
 import type { Resolvers, FileBagCreate } from '@via-profit-services/file-storage';
 
 const uploadResolver: Resolvers['FileStorageMutation']['upload'] = async (
   _parent, args, context,
 ) => {
   const { files, info } = args;
-  const { logger, services } = context;
+  const { logger, services, dataloader } = context;
+
+  // check to exists
+  // new IDs
+  const ids = info.reduce((list, data) => data.id ? list.concat([data.id]) : list, []);
+  // try to load
+  const existFiles = await Promise.all(ids.map((id) => dataloader.files.load(id)));
+
+  //check result
+  const hasFiles = existFiles.filter((data) => data !== null);
+
+  if (hasFiles.length) {
+    throw new BadRequestError(
+      `One or more files that you are trying to upload already exists. Check this IDs: [${hasFiles.map(({ id }) => id).join(', ')}].`,
+    );
+  }
 
   const filesData = await Promise.all(files);
 
@@ -30,6 +46,8 @@ const uploadResolver: Resolvers['FileStorageMutation']['upload'] = async (
       `File «${filename}» uploaded successfully as «${absoluteFilename}»`,
       { mimeType },
     );
+
+    dataloader.files.clear(id);
 
     return {
       id,
